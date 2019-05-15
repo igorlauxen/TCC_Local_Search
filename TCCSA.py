57a9c7f283a5d5d4a95482263584b3b7fcb94e65
igorlauxen <lauxen15@gmail.com>
2019-05-14 21:19:02 -0300

    Update penalty calculation

--- TCCSA.py
+++ TCCSA.py
@@ -12,6 +12,7 @@ import json
 # parametros para mudar a temperatura: uso e ordem
 # maior numero de uso e menor for a ordem mais energia
 num_fields = 5  # numero total de campos
+nrOfUsers = 10  # número de usuários
 num_cost_increases = 100  # valor de aumento do custo
 avg_cost_increase = 200  # valor médio de aumento do custo
 acc_ratio = 0.75  # taxa de aceitação (acceptance ratio) entre 0 e 1
@@ -19,7 +20,6 @@ prob_e = 0.00000000001  # fator de probabilidade
 beta = 0.125  # valor de 0< beta < 1
 max_iter = 4 * num_fields  # número maximo de iterações é o número de fields * 4
 num_temp = 200  # temperatura
-nrOfUsers = 10  # número de usuários
 
 # calculo de temperatura incial
 initial_temperature = avg_cost_increase / math.log(
@@ -30,10 +30,10 @@ final_temperature = -beta * avg_cost_increase / math.log(prob_e)
 alpha = math.pow(final_temperature / initial_temperature, 1 / num_temp)
 
 # considerando 10 usuários
-field1 = {"times": 10, "avg_order": 4}
+field1 = {"times": 8, "avg_order": 4}
 field2 = {"times": 1, "avg_order": 5}
 field3 = {"times": 7, "avg_order": 3}
-field4 = {"times": 0, "avg_order": 2}
+field4 = {"times": 2, "avg_order": 2}
 field5 = {"times": 8, "avg_order": 1}
 
 # estado inicial
@@ -92,7 +92,8 @@ def value(state):
         # nro de vezes que o campo é acessado é menor que o número mínimo de usuários
         if ( position >= avgOrder and times < minimalUsers ) or ( times < minimalUsers ):
             penalty = len(state) - i  # Aplica penalidade como o tamanho do estado +1
-            energy += penalty + 1  # soma penalidade + 1 ao total de energia
+            print('Penalty resulted by ' + str(len(state)) + '-' + str(i) + ' resulted in ' + str(penalty))
+            energy += penalty  # soma penalidade + 1 ao total de energia
         # [BOM+] Se nro de vezes que o campo é acessado é maior que o número mínimo de usuários
         elif times > halfOrMoreUsersUsedField:
             energy += 1  # soma 1 ao total de energia
@@ -100,6 +101,8 @@ def value(state):
         # nro de vezes que o campo é acessado é maior que o número mínimo de usuários
         elif position <= avgOrder and times > minimalUsers:
             energy += 1
+
+        print('This is my result: '+str(energy))
 
     return energy
 
